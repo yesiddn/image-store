c1a81350b63dc4e6fceda54dc93d80550cbc9ef5
Yesid <yesidrodriguez305@gmail.com>
2023-07-11 23:48:57 -0500

    Add home structure

--- views/moduls/home.php
+++ views/moduls/home.php
@@ -1,1 +1,76 @@
-<h1>home</h1>
+<div class="container mt-5">
+  <div class="mb-5 text-center">
+    <h1>Obras de arte</h1>
+    <p>
+      Sumérgete en el fascinante mundo del arte en nuestra exclusiva galería en línea. Aquí encontrarás una selección cuidadosamente curada de obras maestras y creaciones contemporáneas de artistas reconocidos a nivel mundial. Explora la diversidad de estilos, técnicas y expresiones artísticas que hemos reunido para ti. Desde pinturas vibrantes hasta esculturas impresionantes, cada pieza exhibida transmite una historia única y emocionante. Nuestra galería está dedicada a promover el arte en todas sus formas y conectar a los amantes del arte con las obras que los cautivan. Ya seas un coleccionista experimentado, un entusiasta del arte o simplemente alguien que busca inspiración, aquí encontrarás algo que te emocionará.
+    </p>
+  </div>
+
+  <div class="row mb-3">
+    <div class="w-50 offset-md-3 input-group mb-3">
+      <div class="input-group-prepend">
+        <label class="input-group-text" for="inputGroupSelect01">Categoria</label>
+      </div>
+      <select class="form-select" id="inputGroupSelect01">
+        <option selected>Choose...</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-3 mb-4">
+      <div class="card">
+        <img class="card-img-top px-3" src="./views/img/image.png" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">Picture title</h5>
+          <p class="card-text">Autor:</p>
+          <p class="card-text">Fecha publicación: 00/00/0000</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card">
+        <img class="card-img-top px-3" src="./views/img/image.png" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">Picture title</h5>
+          <p class="card-text">Autor:</p>
+          <p class="card-text">Fecha publicación: 00/00/0000</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card">
+        <img class="card-img-top px-3" src="./views/img/image.png" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">Picture title</h5>
+          <p class="card-text">Autor:</p>
+          <p class="card-text">Fecha publicación: 00/00/0000</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-md-3 mb-4">
+      <div class="card">
+        <img class="card-img-top px-3" src="./views/img/image.png" alt="Card image">
+        <div class="card-body">
+          <h5 class="card-title">Picture title</h5>
+          <p class="card-text">Autor:</p>
+          <p class="card-text">Fecha publicación: 00/00/0000</p>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
